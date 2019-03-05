daf6a3bd8c8188b7316099ec1f8380ba4e431e51
Aki Teshima <tteshima@nvidia.com>
2019-03-05 16:34:04 +0900

    run threads and exit

--- main.cpp
+++ main.cpp
@@ -15,7 +15,15 @@ struct message_t {
     unsigned int* pCounter;
 };
 typedef struct message_t message;
+message baseLocation;
 
+void raceCountUp(const message* m)
+{
+    unsigned int *lock = m->pLock;
+    //unsigned int *counter = m->pCounter;
+    while(*lock == 0);
+    return;
+}
 
 int main(int argc, char** argv)
 {
@@ -37,5 +45,25 @@ int main(int argc, char** argv)
     maxIteration = 0 == maxIteration ? 1 : maxIteration;
     std::cout << "threadNumber: " << threadNumber << std::endl;
     std::cout << "maxIteration: " << maxIteration << std::endl;
+
+    baseLocation.pLock = &gLock;
+    baseLocation.pCounter = &gCounter;
+    gLock = 0;
+    gCounter = 0;
+
+    std::vector<std::thread> vecThread;
+    for(unsigned int i = 0;i < threadNumber;i++)
+    {
+        vecThread.push_back(std::thread(raceCountUp, &baseLocation));
+    }
+
+    getchar();
+    gLock = 1;
+
+    for(auto&& it : vecThread)
+    {
+        it.join();
+    }
+
     return 0;
 }
